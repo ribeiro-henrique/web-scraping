78c52e4a1e483bfbf765e1b5131bf2ec0e4fa831
Henrique <hesr.ribeiro@gmail.com>
2024-04-24 22:26:58 -0300

    add rmais get

--- src/medcof/medcof.controller.ts
+++ src/medcof/medcof.controller.ts
@@ -4,8 +4,13 @@ import { MedCofService } from "./medcof.service";
 @Controller('medcof')
 export class MedCofController {
     constructor(private medCofService: MedCofService ) {}
-    @Get()
-    getData() {
-        return this.medCofService.getPlano();
+    @Get('/rmais')
+    getRmais() {
+        return this.medCofService.getRmais();
+    }
+
+    @Get('/rum')
+    getRUm() {
+        return this.medCofService.getRUm();
     }
 }
